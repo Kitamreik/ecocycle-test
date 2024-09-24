2dddfe3c5306b9592a2bc4179d7e1eca499e01ea
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-09-24 00:48:57 -0500

    moved the user operations to userRoutes

---
+++ routes/userRoutes.js
@@ -0,0 +1,22 @@
+﻿const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+// Middleware to check if user is authenticated
+const isAuthenticated = (req, res, next) => {
+    if (req.session.isAuthenticated) {
+        next();
+    } else {
+        res.redirect('/admin/login');
+    }
+};
+
+// Routes related to users
+router.get('/api/users', isAuthenticated, userController.getUsers);
+router.get('/api/users/add', userController.addUserForm);
+router.post('/api/users/add', isAuthenticated, userController.addUser);
+router.get('/api/users/edit/:userId', isAuthenticated, userController.editUserForm);
+router.put('/api/users/:userId', isAuthenticated, userController.updateUser);
+router.delete('/api/users/:userId', isAuthenticated, userController.deleteUser);
+
+module.exports = router;
