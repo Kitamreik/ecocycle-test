2516aa06f2e50309c61cbe5de6c41255eb5de1fe
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-09-24 15:19:08 -0500

    removed session handling for the admin routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,34 +2,14 @@
 const express = require('express');
 const router = express.Router();
 
-// Dummy credentials for demonstration
-const adminCredentials = {
-    username: process.env.ADMIN_USERNAME,
-    password: process.env.ADMIN_PASSWORD
-};
-
 // Middleware to check if user is authenticated
 const isAuthenticated = (req, res, next) => {
-    console.log('isAuthenticated middleware - Session:', req.session);
-    if (req.session.isAuthenticated) {
-        console.log('User is authenticated');
-        next();
-    } else {
-        console.log('User is not authenticated, redirecting to login');
-        res.redirect('/admin/login');
-    }
+    next();
 };
 
 // Middleware to redirect authenticated users
 const redirectIfAuthenticated = (req, res, next) => {
-    console.log('redirectIfAuthenticated middleware - Session:', req.session);
-    if (req.session.isAuthenticated) {
-        console.log('User is already authenticated, redirecting to panel');
-        res.redirect('/admin/panel');
-    } else {
-        console.log('User is not authenticated, proceeding to login page');
-        next();
-    }
+    next();
 };
 
 // Render login page
@@ -46,14 +26,8 @@ router.get('/login', redirectIfAuthenticated, (req, res) => {
 router.post('/login', (req, res) => {
     const { username, password } = req.body;
     console.log('Login attempt:', { username, password });
-    if (username === adminCredentials.username && password === adminCredentials.password) {
-        req.session.isAuthenticated = true;
-        console.log('Login successful, redirecting to panel');
-        res.redirect('/admin/panel');
-    } else {
-        console.log('Login failed, redirecting to password error');
-        res.redirect('/admin/password-error');
-    }
+    // Here, you can implement your own login logic without session management
+    res.redirect('/admin/panel');
 });
 
 // Admin panel route
@@ -65,16 +39,6 @@ router.get('/panel', isAuthenticated, (req, res) => {
 // Password error route
 router.get('/password-error', (req, res) => {
     res.render('pages/admin-err');
-});
-
-// Logout route
-router.get('/logout', (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            console.error('Error destroying session:', err);
-        }
-        res.redirect('/admin/login');
-    });
 });
 
 // Define API routes to serve EJS templates with data from Supabase
@@ -155,6 +119,7 @@ router.get('/api/schools', isAuthenticated, async (req, res) => {
         });
     }
 });
+
 router.get('/api/calendar', async (req, res) => {
     try {
         const monthQuery = parseInt(req.query.month);
