e33e8addd10f25273cc9b6bde91dc43e33614031
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-10-26 01:02:50 -0500

    minor update.

--- routes/requestRoutes.js
+++ routes/requestRoutes.js
@@ -4,23 +4,15 @@ const requestController = require('../controllers/requestController');
 
 // Middleware to check if the user is authenticated
 const isAuthenticated = (req, res, next) => {
-    // if (req.session.isAuthenticated) {
-    //     next();
-    // } else {
-    //     res.redirect('/admin/login');
-    // }
     next();
 };
 
 // Get all requests
 router.get('/api/requests', isAuthenticated, requestController.getRequests);
-router.get('/add', isAuthenticated, requestController.addRequestForm);
-router.post('/add', isAuthenticated, requestController.addRequest);
-router.get('/edit/:requestId', isAuthenticated, requestController.editRequestForm);
-router.post('/edit/:requestId', isAuthenticated, (req, res, next) => {
-    req.method = 'PUT';
-    next();
-}, requestController.updateRequest);
-router.delete('/delete/:requestId', isAuthenticated, requestController.deleteRequest);
+router.get('/api/requests/add', isAuthenticated, requestController.addRequestForm);
+router.post('/api/requests/add', isAuthenticated, requestController.addRequest);
+router.get('/api/requests/edit/:requestId', isAuthenticated, requestController.editRequestForm);
+router.put('/api/requests/:requestId', isAuthenticated, requestController.updateRequest);
+router.delete('/api/requests/:requestId', isAuthenticated, requestController.deleteRequest);
 
 module.exports = router;
